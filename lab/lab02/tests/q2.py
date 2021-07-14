1e0f4a8a24ce14a97c93e4f55b5c0582643ec753
Isaac Merritt <isaacmerritt@berkeley.edu>
2021-07-13 22:23:36 -0700

    lab02 upload

---
+++ lab/lab02/tests/q2.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2',
+    'points': 0,
+    'suites': [   {   'cases': [   {'code': ">>> starts_with_vowels(some_words) == ['alligator', 'aardvark', 'owl', 'unicorn']\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> starts_with_vowels(["bear"]) == []\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> "zebra" not in starts_with_vowels(some_words)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab02/tests/q3.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q3',
+    'points': 0,
+    'suites': [   {   'cases': [   {'code': '>>> acronym("The University of California, Berkeley") == "UCB"\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> acronym("") == ""\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> acronym("Data Science at Berkeley") == "DSB"\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> acronym("The United States of America") == "USA"\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
