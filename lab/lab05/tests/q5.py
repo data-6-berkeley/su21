819f46e766d6b0930885575655adbc653bd5a0ff
castroian <42158157+castroian@users.noreply.github.com>
2021-07-26 09:24:57 -0700

    add lab5

---
+++ lab/lab05/tests/q5.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q5',
+    'points': 0,
+    'suites': [{'cases': [{'code': '>>> round(bowl_eligible, 3) == 0.581\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
